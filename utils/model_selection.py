98ea1c786bf293670192c045a55ee57d4a765ce5
niucheng1991 <niucheng1991@gmail.com>
2018-11-21 00:31:01 +0800

    添加功能函数

--- utils/model_selection.py
+++ utils/model_selection.py
@@ -1,6 +1,28 @@
 from random import randrange
 from random import seed
+from math import sqrt
 import numpy as np
+
+def shuffle_data(X, y, seed=None):
+    if seed:
+        np.random.seed(seed)
+    idx = np.arange(X.shape[0])
+    np.random.shuffle(idx)
+    return X[idx], y[idx]
+
+def batch_iterator(X, y=None, batch_size=64):
+    n_samples = X.shape[0]
+    for i in np.arange(0, n_samples, batch_size):
+        begin, end = i, min(i + batch_size, n_samples)
+        if y is not None:
+            yield X[begin:end], y[begin:end]
+        else:
+            yield X[begin:end]
+
+
+
+def to_nominal(x):
+    return np.argmax(x, axis=1)
 
 def train_test_split(X, y, test_size=0.6):
 
@@ -20,10 +42,35 @@ def train_test_split(dataset, split=0.6):
     return train, dataset_copy
 
 
-# def k_fold_cross_validation_sets(X, y):
-#     n_samples = len(y)
-#     left_overs = {}
-#     n_left_overs = (n_samples % k)
+def k_fold_cross_validation_sets(X, y, k, shuffle=True):
+    """ Split the data into k sets of training / test data """
+    if shuffle:
+        X, y = shuffle_data(X, y)
+
+    n_samples = len(y)
+    left_overs = {}
+    n_left_overs = (n_samples % k)
+    if n_left_overs != 0:
+        left_overs["X"] = X[-n_left_overs:]
+        left_overs["y"] = y[-n_left_overs:]
+        X = X[:-n_left_overs]
+        y = y[:-n_left_overs]
+
+    X_split = np.split(X, k)
+    y_split = np.split(y, k)
+    sets = []
+    for i in range(k):
+        X_test, y_test = X_split[i], y_split[i]
+        X_train = np.concatenate(X_split[:i] + X_split[i + 1:], axis=0)
+        y_train = np.concatenate(y_split[:i] + y_split[i + 1:], axis=0)
+        sets.append([X_train, X_test, y_train, y_test])
+
+    # Add left over samples to last set as training samples
+    if n_left_overs != 0:
+        np.append(sets[-1][0], left_overs["X"], axis=0)
+        np.append(sets[-1][2], left_overs["y"], axis=0)
+
+    return np.array(sets)
 
 
 def cross_validation_split(dataset, folds=3):
@@ -38,7 +85,68 @@ def cross_validation_split(dataset, folds=3):
         dataset_split.append(fold)
     return dataset_split
 
-seed(1)
-dataset = [[1], [2], [3], [4], [5], [6], [7], [8], [9], [10]]
-folds = cross_validation_split(dataset, 4)
-print(folds)
+
+# calculate accuracy percentage between two lists
+def accuracy_metric(actual, predicted):
+    correct = 0
+    for i in range(len(actual)):
+        if actual[i] == predicted[i]:
+            correct += 1
+    return correct / float(len(actual)) * 100.0
+
+# calculate a confusion matrix
+def confusion_matrix(actual, predicted):
+    unique = set(actual)
+    matrix = [list() for x in range(len(unique))]
+    for i in range(len(unique)):
+        matrix[i] = [0 for x in range(len(unique))]
+    lookup = dict()
+    for i, value in enumerate(unique):
+        lookup[value] = i
+    for i in range(len(actual)):
+        x = lookup[actual[i]]
+        y = lookup[predicted[i]]
+        matrix[x][y] += 1
+    return unique, matrix
+
+# pretty print a confusion matrix
+def print_confusion_matrix(unique, matrix):
+    print('(P)' + ' '.join(str(x) for x in unique))
+    print('(A)---')
+    for i, x in enumerate(unique):
+        print("%s| %s" % (x, ' '.join(str(x) for x in matrix[i])))
+
+# Test confusion matrix with integers
+# actual = [0,0,0,0,0,1,1,1,1,1]
+# predicted = [0,1,1,0,0,1,0,1,1,1]
+# unique, matrix = confusion_matrix(actual, predicted)
+# print_confusion_matrix(unique, matrix)
+
+
+# calculate mean absolute error
+def mae_metric(actual, predicted):
+    sum_error = 0.0
+    for i in range(len(actual)):
+        sum_error += abs(predicted[i] - actual[i])
+    return sum_error / float(len(actual))
+
+# Test RMSE
+actual = [0.1, 0.2, 0.3, 0.4, 0.5]
+predicted = [0.11, 0.19, 0.29, 0.41, 0.5]
+mae = mae_metric(actual, predicted)
+print(mae)
+
+# calculate root mean squared error
+def rmse_metric(actual, predicted):
+    sum_error = 0.0
+    for i in range(len(actual)):
+        predicted_error = predicted[i] - actual[i]
+        sum_error += (predicted_error ** 2)
+    mean_error = sum_error / float(len(actual))
+    return sqrt(mean_error)
+
+# Test RMSE
+actual = [0.1, 0.2, 0.3, 0.4, 0.5]
+predicted = [0.11, 0.19, 0.29, 0.41, 0.5]
+rmse = rmse_metric(actual, predicted)
+print(rmse)
